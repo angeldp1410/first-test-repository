136512f0db5c0a6d9fa8ed5ae93fb5fa5be177f3
Angel <angeldp1410@gmail.com>
2025-07-08 15:27:01 -0600

    Quinto papu commit

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,16 @@
 #Escribir un programa que almacene la cadena de caracteres contraseña en una variable,
-#pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.
+#pregunte al usuario la contraseña y al alcanzar los 3 intentos este se bloquee.
 
 pasword = 'papugit'
 
 text_input = input('Por favor ingrese su contraseña')
 
+sentinel = 0
+
 
 
 while text_input != pasword:
     text_input = input('Por favor ingrese su contraseña')
+    sentinel += 1
     
 print('Contraseña correcta :)')
