8d303db11396ed7f814ab0269ee1d7e234bb1c75
Angel <angeldp1410@gmail.com>
2025-07-07 20:57:20 -0600

    Este es el segundo papu commit

--- ejercicio.py
+++ ejercicio.py
@@ -1,3 +1,4 @@
 #Escribir un programa que almacene la cadena de caracteres contraseña en una variable,
 #pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.
 
+pasword = input('Ingrese su contraseña porfavor')
